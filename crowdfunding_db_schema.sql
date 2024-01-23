81b4d4ced6686726e40024feb9416f626a3fcf82
Clare Kim <clarekim@Clares-MacBook-Air-2.local>
2024-01-22 23:40:22 -0500

    first

---
+++ crowdfunding_db_schema.sql
@@ -0,0 +1,41 @@
+CREATE TABLE Campaign (
+    cf_id VARCHAR(255) PRIMARY KEY,
+    contact_id VARCHAR(255),
+    company_name VARCHAR(255),
+    description TEXT,
+    goal DECIMAL(10, 2),
+    pledged DECIMAL(10, 2),
+    outcome VARCHAR(255),
+    backers_count INT,
+    country VARCHAR(255),
+    currency VARCHAR(255),
+    launch_date DATE,
+    end_date DATE,
+    category_id VARCHAR(255),
+    subcategory_id VARCHAR(255)
+);
+
+SELECT * FROM Campaign 
+
+CREATE TABLE Category (
+    category_id VARCHAR(255) PRIMARY KEY,
+    category VARCHAR(255)
+);
+
+SELECT * FROM Category 
+
+CREATE TABLE Subcategory (
+    subcategory_id VARCHAR(255) PRIMARY KEY,
+    subcategory VARCHAR(255)
+);
+
+SELECT * FROM Subcategory 
+
+CREATE TABLE Contacts (
+    contact_id INT PRIMARY KEY,
+    first_name VARCHAR(255),
+    last_name VARCHAR(255),
+    email VARCHAR(255)
+);
+
+SELECT * FROM Contacts
